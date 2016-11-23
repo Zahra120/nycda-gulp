c5ee4c073820abf9605342be91b8a908e9137e12
Robert <robertmet1b@gmail.com>
2016-11-23 13:44:48 +0100

    initial setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express'),
+    logger = require('morgan');
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, () => {
+  console.log('Web server listening on port 3000');
+});
